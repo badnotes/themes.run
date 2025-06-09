9c762f1b61865f40943b55145340907ef53ab069
jun.wan <jun.wan@chinascope.com>
2025-06-09 10:31:49 +0800

    up post

--- src/content.config.ts
+++ src/content.config.ts
@@ -23,6 +23,13 @@ const blog = defineCollection({
 			license: z.string().optional(), // 许可
 			tags: z.array(z.string()).optional(), //标签
 			overview: z.string().optional(),
+			css: z.string().optional(), // CSS
+			ui: z.string().optional(), // UI
+			ssg: z.string().optional(), // SSG 同上面type，此字段暂时未使用
+			cat: z.string().optional(), // 分类- Blog/Dashboard/Documentation/Bussiness/Landing Page/Portfolio/Product/Starter Template/Theme
+			distribution: z.string().optional(), // 分发-OpenSource/Free/Premium
+			blueprint: z.string().optional(), // 蓝图 - Template&Themes/UI/Components
+			cms: z.string().optional(), // CMS - 内容管理系统
 		})
 })
 
